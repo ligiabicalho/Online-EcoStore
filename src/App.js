3e4661b06dc9e417bcf544bcd78db51b841239b9
Joana <joanamdsantos1@gmail.com>
2022-10-07 17:12:07 -0300

    fazendo as modificações solicitadas do code review

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import { getCategories } from './services/api';
 
 // Iniciando o projeto group-3-release
 
 class App extends React.Component {
   render() {
-    console.log(getCategories());
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <p>Edit src/App.js and save to reload.</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter />
     );
   }
 }
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,6 @@ export async function getCategories() {
   const endpoint = 'https://api.mercadolibre.com/sites/MLB/categories';
   const response = await fetch(endpoint);
   const data = await response.json();
-  console.log(data);
   return data;
 }
 
