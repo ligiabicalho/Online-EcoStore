8fca5b66731e377b8acf23168123f9b024719e2f
Lígia Bicalho <ligiabicalho@gmail.com>
2022-10-18 11:51:36 -0300

    14. Limite a quantidade de produtos adicionados ao carrinho pela quantidade disponível em estoque

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -69,6 +69,9 @@ class ShoppingCart extends React.Component {
                       id={ product.id }
                       type="button"
                       data-testid="product-increase-quantity"
+                      disabled={
+                        (product.quantity === product.available_quantity)
+                      }
                       onClick={ this.handleQuantity }
                     >
                       +
